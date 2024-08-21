f6726e9204e6f140a941200ba9275fcd9c24412b
amey1355 <assawant06@gmail.com>
2024-08-21 12:16:23 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero"
+import SuperQuality from "./SuperQuality"
+import Subscribe from "./Subscribe"
+import SpecialOffers from "./SpecialOffers"
+import Services from "./Services"
+import PopularProducts from "./PopularProducts"
+import CustomerReviews from "./CustomerReviews"
+import Footer from "./Footer"
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
